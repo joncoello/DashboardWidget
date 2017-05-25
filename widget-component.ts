9bfe40bc6ec46552bcec45fa1311f46691858149
Jon Coello <joncoello@gmail.com>
2017-05-25 13:17:22 +0100

    add widget component

---
+++ widget-component.ts
@@ -0,0 +1,40 @@
+﻿class WidgetComponenet {
+    
+    constructor(public element: Element, public settings: WidgetSettings) {
+        settings.loadData(element);
+    }
+
+}
+
+class WidgetSettings {
+    public loadData: (element: Element) => void;
+}
+
+class WidgetManager {
+
+    // singleton implementation
+    private static _instance: WidgetManager;
+
+    private constructor() {
+        this._widgets = new Array<WidgetComponenet>();
+    }
+
+    public static get Instance() : WidgetManager {
+        return this._instance || (this._instance = new this());
+    }
+    
+    // registry of widgets on a page
+    private _widgets: Array<WidgetComponenet>
+
+    // add a widget to the collection
+    public registerWidget(widget: WidgetComponenet): void {
+        this._widgets.push(widget);
+    }
+
+    public RefreshWidgets(): void {
+        this._widgets.forEach((w: WidgetComponenet) => {
+            w.settings.loadData(w.element);
+        });
+    }
+    
+}
